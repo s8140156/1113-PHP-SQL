53bcb4502cffc898a58af653f51b778de0632847
s8140156 <s8140156@gmail>
2023-11-13 16:34:49 +0800

    1113member session

--- login.php
+++ login.php
@@ -1,27 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>會員登入</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</head>
-<body>
-	<h1 class="text-center">登入</h1>
-	<div d-flex>
-		<div class="mb-3 col-4 m-auto"> 
-				<label for="" class="form-label">帳號:</label>
-				<input type="text" class="form-control" name="acc" id="acc">
-			</div>
-			<div class="mb-3 col-4 m-auto">
-				<label for="" class="form-label">密碼:</label>
-				<input type="password" class="form-control" name="pw" id="pw">
-			</div>
-			<div class="mb-3 col-4 m-auto">
-				<input type="submit" value="submit">
-				<input type="reset" value="clear">
-			</div>
-	</div>
-</body>
-</html>
+<?php
+SESSION_start();
+
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+
+$dsn="mysql:host=localhost;charset=utf8;dbname=member";
+$pdo=new PDO($dsn,'root','');
+
+// $sql="select * from users where `acc`=`$acc` && `$pw`";
+$sql="select count(*) from users where `acc`=`$acc` && `$pw`";
+// $user=$pdo->query($sql)->fetch();
+$user=$pdo->query($sql)->fetchColumn();
+// print_r($user);
+// 確認傳回的資訊是甚麼 返回該筆資料中指定欄位的資料，$n為欄位的索引值(0,1,2…)
+echo $sql;
+exit();
+// 以上是檢查機制 看自己sql寫了甚麼
+// exit();是強制php執行到這邊
+
+// if($user['acc']==$acc && $user['pw']==$pw){
+// if($user==1){
+if($user){
+	$_SESSION['user']=$acc;
+	header("location:index.php");
+}else{
+	header('location:login_form.php?error=帳號密碼錯誤');
+}
+
+?>
