0e4d6db4da4531fe80ecfc1ba1125413ee0f811f
s8140156 <s8140156@gmail>
2023-11-17 15:03:55 +0800

    自訂函式應用

--- function/index1.php
+++ function/index1.php
@@ -1,3 +1,4 @@
+<h1>自訂函式</h1>
 <?php
 // 自訂函式
 
@@ -30,3 +31,55 @@ echo "總和是:" .sum(21,58);
 ?>
 
 <!-- 自訂函式只執行funcion{}裡面 全域的不管 -->
+<h1>不定參數用法</h1>
+<?php
+
+function sum2(...$arg){
+	// print_r($arg);
+	// $arg是一個陣列
+	$sum=0;
+	foreach($arg as $num){
+		if(is_numeric($num)){
+			$sum +=$num;
+		}
+	}
+	return $sum;
+}
+
+echo sum2(1,2);
+echo "<hr>";
+echo sum2(5,10);
+echo "<hr>";
+echo sum2(1,3,5,'xhjijo',9,11);
+echo "<hr>";
+
+// 思考使用者怎麼使用你的東西
+
+
+// function sum2($a,$b,$c){
+
+// 	$sum=$a+$b+$c;
+// 	echo "輸入:".$a. "、".$b ."、";
+// 	echo"<br>";
+// 	return $sum;
+// }
+
+?>
+
+<h1>自訂函式預設值</h1>
+<?php
+
+function sum3($a,$b,$c=3){
+	$sum=($a+$b)*$c;
+	echo "$a 、 $b , 倍數 $c <br>";
+	return $sum;
+
+}
+
+echo sum3(10,15);
+echo "<hr>";
+echo sum3(10,15,10);
+echo "<hr>";
+
+
+?>
